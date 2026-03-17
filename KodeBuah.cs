dc0d7737077bb29c1b3f51d5966ac49bbf30dab8
dynastygunawan22 <dynastygunawan22@gmail.com>
2026-03-17 15:44:04 +0700

    Menambahkan class KodeBuah

---
+++ KodeBuah.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace modul4_103082400037
+{
+    internal class KodeBuah
+    {
+    }
+}
--- obj/Debug/net10.0/modul4_103082400037.AssemblyInfo.cs
+++ obj/Debug/net10.0/modul4_103082400037.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("modul4_103082400037")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52de36cce9823ef471d48febe4ba6432a6e68717")]
 [assembly: System.Reflection.AssemblyProductAttribute("modul4_103082400037")]
 [assembly: System.Reflection.AssemblyTitleAttribute("modul4_103082400037")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
